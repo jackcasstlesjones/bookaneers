56034cd50a10ed610ed432b71ca64739c383f1bb
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2024-10-05 14:13:12 +0100

    create component structure

---
+++ src/Components/Sidebar/Sidebar.tsx
@@ -0,0 +1,9 @@
+const Sidebar = () => {
+  return (
+    <div className=" h-screen bg-slate-800 w-96">
+      <h1 className=" text-white text-7xl">MyLib</h1>
+    </div>
+  );
+};
+
+export default Sidebar;
