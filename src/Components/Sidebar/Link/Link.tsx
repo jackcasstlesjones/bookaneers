9ee9934ec4eb1cc3c5a8283204e90de80e61e938
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2024-10-05 15:12:29 +0100

    feat: add sidebar

---
+++ src/Components/Sidebar/Link/Link.tsx
@@ -0,0 +1,9 @@
+const Link = ({ linkText }) => {
+  return (
+    <a className="text-4xl" href="">
+      {linkText}
+    </a>
+  );
+};
+
+export default Link;
--- src/Components/Sidebar/Sidebar.tsx
+++ src/Components/Sidebar/Sidebar.tsx
@@ -1,7 +1,16 @@
+import Link from "./Link/Link";
+
 const Sidebar = () => {
+  const linkNames = ["All Books", "Authors", "Genres"];
+
   return (
     <div className=" h-screen bg-slate-800 w-96">
-      <h1 className=" text-white text-7xl">MyLib</h1>
+      <div className="flex flex-col text-white gap-9 m-auto w-3/4">
+        <h1 className=" text-white text-7xl mt-20 mb-20">MyLib</h1>
+        {linkNames.map((element) => {
+          return <Link linkText={element} />;
+        })}
+      </div>
     </div>
   );
 };
