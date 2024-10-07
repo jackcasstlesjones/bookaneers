5a23fbdf54d9382bc75b9736bd30ddaf5f4fa43c
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2024-10-07 12:07:26 +0100

    refactor: add props interface to Card and Image components

--- src/Components/CardsContainer/Card/Image/Image.tsx
+++ src/Components/CardsContainer/Card/Image/Image.tsx
@@ -1,4 +1,8 @@
-const Image = ({ source }) => {
+interface ImageProps {
+  source: string;
+}
+
+const Image = ({ source }: ImageProps) => {
   return <img className="mb-5 w-44" src={source} alt="" />;
 };
 
